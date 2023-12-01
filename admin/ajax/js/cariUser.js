5aeb9a595a9399e2806d1296cc564014bbb8a653
daffalahidayat <111967058+daffalahidayat@users.noreply.github.com>
2023-12-01 23:10:42 +0700

    Delete admin directory

--- admin/ajax/js/cariUser.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    // event ketika keyword diketik
-    $('#keyword').on('keyup', function () {
-        console.log('ok cari');
-        $('.tampil').load('ajax/cariUser?keyword=' + $('#keyword').val());
-    });
-});
